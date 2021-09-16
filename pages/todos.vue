a422df2f447d2ce63b41ecee1686e26060f51b82
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2021-09-16 11:17:17 +0900

    add: TODO追加機能実装

---
+++ pages/todos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <form v-on:submit.prevent="add">
+        <input v-model="name">
+        <button>Add</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        name: '',
+        done: false,
+      }
+    },
+    created: function() {
+      this.$store.dispatch('todos/init')
+    },
+    methods: {
+      add() {
+        this.$store.dispatch('todos/add', this.name)
+        this.name = ''
+      }
+    }
+  }
+</script>
