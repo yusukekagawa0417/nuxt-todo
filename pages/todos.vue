fe5170520c8c4fd90a11e72e1384b82277555ac5
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2021-09-17 21:00:29 +0900

    update: 日時フォーマットを変更

--- pages/todos.vue
+++ pages/todos.vue
@@ -2,14 +2,16 @@
   <div>
     <ul>
       <li v-for="todo in todos" :key="todo.id">
-        <input
-        type="checkbox"
-        v-bind:checked="todo.done"
-        @change="toggle(todo)">
-        <span v-bind:class="{ done: todo.done }">
-          {{ todo.name }} {{ todo.created }}
+        <span v-if="todo.created">
+          <input
+          type="checkbox"
+          v-bind:checked="todo.done"
+          @change="toggle(todo)">
+          <span v-bind:class="{ done: todo.done }">
+            {{ todo.name }} {{ todo.created.toDate() | dateFilter }}
+          </span>
+          <button v-on:click="remove(todo.id)">X</button>
         </span>
-        <button v-on:click="remove(todo.id)">X</button>
       </li>
     </ul>
     <div>
@@ -22,6 +24,7 @@
 </template>
 
 <script>
+import moment from 'moment'
   export default {
     data: function() {
       return {
@@ -48,12 +51,17 @@
       todos() {
         return this.$store.state.todos.todos
       }
+    },
+    fileters: {
+      dateFilter: function(date) {
+        return  moment(date).format('YYYY/MM/DD HH:mm:ss')
+      }
     }
   }
 </script>
 
 <style>
-li >span.done {
+li > span > span.done {
   text-decoration: line-through;
 }
 </style>
